aab6c241e1290e00f89fee07aed64c2047bcb66b
Sanotsch2003 <jonas.mueller.wpk@gmail.com>
2026-02-15 02:05:06 +0100

    Added Camera Controller, cleaned up project, experimented with instancing

--- src/main.rs
+++ src/main.rs
@@ -6,3 +6,21 @@ fn main() {
         eprintln!("Error: {}", e);
     }
 }
+
+// Testing
+// struct Point<T> {
+//     x: T,
+//     y: T,
+// }
+//
+// impl<T> Point<T> {
+//     fn new(x: T, y: T) -> Self {
+//         Point { x, y }
+//     }
+// }
+//
+// fn main() {
+//     let x : f64 = 3.14;
+//
+// }
+
